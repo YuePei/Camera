c7b43d955064b354044b83fe16a53e1dcdc3664e
占益民 <allen@peytondeimac.local>
2018-05-28 14:32:45 +0800

    Initial Commit

---
+++ Camera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Camera
+//
+//  Created by Peyton on 2018/5/28.
+//  Copyright © 2018年 Peyton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
